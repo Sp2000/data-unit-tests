5cfe8ffa8558ec6645bcd67c428ec88bf9c91997
Geoff Ower <gdower@illinois.edu>
2019-01-08 17:09:04 -0600

    Cleaning up

--- scripts/build.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-exclude = ["../coldp/", "../scripts/", "../template/", "0"]
-directories = os.system("ls -d ../*/")
-print(directories)
-
-for dir in directories:
-    print(str(dir))
